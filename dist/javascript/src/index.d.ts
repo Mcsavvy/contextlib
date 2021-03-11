605fdf9b15ee74e043954db6871800149290a0ac
mcsavvy <phishink5@gmail.com>
2021-03-11 18:56:27 +0100

    Deleted dist

--- dist/javascript/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-import With from './contextlib';
-export default With;
-export * from './contextlib';
-export { ContextManager as AsyncContextManager, With as AsyncWith, Use as AsyncUse, ExitStack as AsyncExitStack } from './async';
--- dist/javascript/src/index.js
+++
@@ -1,5 +0,0 @@
-import With from './contextlib';
-export default With;
-export * from './contextlib';
-export { With as AsyncWith, Use as AsyncUse, ExitStack as AsyncExitStack } from './async';
-//# sourceMappingURL=index.js.map
