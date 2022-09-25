56780e4fc4a346f8e89f0ad4368594f28d6cf6da
mcsavvy <phishink5@gmail.com>
2022-09-25 23:50:28 +0100

    Removed Unnecessary Content From Active Branch

--- src/utils.ts
+++
@@ -1,7 +0,0 @@
-
-export function getattr<T> (object: T, name: string, _default = null) {
-    if (name in object) {
-        return object[name]
-    } else if (_default !== null) return _default
-    throw `AttributeError: ${object} has no attribute "${name}"`
-}
--- test/index.test.ts
+++
@@ -1,8 +0,0 @@
-import * as contextlib from '../src'
-import { AsyncContextManager } from '../src'
-import { ContextManager } from '../src/types'
-import With from '../src/contextlib'
-
-test('imports', () => {
-    expect(typeof With).toBe('function')
-})
